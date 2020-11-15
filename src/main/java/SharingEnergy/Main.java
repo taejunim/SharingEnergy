11b72fe20aadef9ed27ebda2db928d5fbe5d0735
Taejun Lim <tj5054@icloud.com>
2020-11-15 20:34:05 +0900

    initial commit

---
+++ src/main/java/SharingEnergy/Main.java
@@ -0,0 +1,22 @@
+package SharingEnergy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class Main {
+
+
+    /** 로그인 화면 **/
+    @RequestMapping(value = "test.do")
+    public String Login(HttpServletRequest req) throws Exception {
+
+
+        System.out.println("test");
+
+        return "index";
+    }
+
+}
