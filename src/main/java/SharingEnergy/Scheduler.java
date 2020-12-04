af4ac3e25025db2ba7096567cd30c981c79b1e14
Taejun Lim <tj5054@icloud.com>
2020-12-04 18:04:34 +0900

    유영 시작

--- src/main/java/SharingEnergy/Scheduler.java
+++ src/main/java/SharingEnergy/Scheduler.java
@@ -1,8 +1,20 @@
 package SharingEnergy;
 
+import SharingEnergy.Object.OutsideDataObject;
+import SharingEnergy.Object.RequestQuantumDataObject;
+import SharingEnergy.Object.ResponseObject;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.http.converter.FormHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.GsonHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Scheduler {
@@ -14,71 +26,77 @@ public class Scheduler {
     /*매월 1일 0시 30분 명예의전당 입력*/
     @Scheduled(cron="*/10 * * * * *")
     public void HofScheduler() {
-//        try{
-//
-//            // RestTemplate 에 MessageConverter 세팅
-//            List<HttpMessageConverter<?>> converters = new ArrayList<HttpMessageConverter<?>>();
-//            converters.add(new FormHttpMessageConverter());
-//            converters.add(new StringHttpMessageConverter());
-//            converters.add(new MappingJackson2HttpMessageConverter());
-//            converters.add(new GsonHttpMessageConverter());
-//
-//            RestTemplate restTemplate = new RestTemplate();
-//            restTemplate.setMessageConverters(converters);
-//
-////            // parameter 세팅
-////            MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
-////            map.add("str", "thisistest");
-//
-//            ArrayList<RequestQuantumDataObject> requestQuantumDataObjectArrayList = new ArrayList<RequestQuantumDataObject>();
-//
-//            for (int i=0; i<deviceArray.length; i++) {
-//
-//                System.out.println(deviceArray[i]);
-//                RequestQuantumDataObject requestQuantumDataObject = new RequestQuantumDataObject();
-//
-//                requestQuantumDataObject.setStartDttm("2020-12-04 14:40");
-//
-//                //PV
-//                if (deviceArray[i].equals("001")) {
-//                    requestQuantumDataObject.setDeviceGbnCd(deviceArray[0]);
-//                    requestQuantumDataObject.setChgrGbnCd("001");
-//
-//                    requestQuantumDataObjectArrayList.add(requestQuantumDataObject);
-//                }
-//
-//                //ESS
-//                else if (deviceArray[i].equals("004")) {
-//                    requestQuantumDataObject.setDeviceGbnCd(deviceArray[1]);
-//                    requestQuantumDataObject.setChgrGbnCd("001");
-//
-//                    requestQuantumDataObjectArrayList.add(requestQuantumDataObject);
-//
-//                    requestQuantumDataObject.setChgrGbnCd("002");
-//
-//                    requestQuantumDataObjectArrayList.add(requestQuantumDataObject);
-//                }
-//
-//                //EV
-//                else if (deviceArray[i].equals("005")) {
-//                    requestQuantumDataObject.setDeviceGbnCd(deviceArray[2]);
-//                    requestQuantumDataObject.setChgrGbnCd("001");
-//                    requestQuantumDataObjectArrayList.add(requestQuantumDataObject);
-//
-//                }
-//
-//
-//            }
-//
-//            // REST API 호출
-//            ResponseObject responseObject = restTemplate.postForObject("http://172.30.1.51:8080/SharingEnergy/checkLatestData.do", requestQuantumDataObjectArrayList, ResponseObject.class);
-//            System.out.println("------------------ TEST 결과 ------------------");
-//            System.out.println(responseObject);
-//
-//
-//
-//        }catch(Exception e){
-//            e.printStackTrace();
-//        }
+        try{
+
+            // RestTemplate 에 MessageConverter 세팅
+            List<HttpMessageConverter<?>> converters = new ArrayList<HttpMessageConverter<?>>();
+            converters.add(new FormHttpMessageConverter());
+            converters.add(new StringHttpMessageConverter());
+            converters.add(new MappingJackson2HttpMessageConverter());
+            converters.add(new GsonHttpMessageConverter());
+
+            RestTemplate restTemplate = new RestTemplate();
+            restTemplate.setMessageConverters(converters);
+
+//            // parameter 세팅
+//            MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
+//            map.add("str", "thisistest");
+
+            ArrayList<RequestQuantumDataObject> requestQuantumDataObjectArrayList = new ArrayList<RequestQuantumDataObject>();
+
+            OutsideDataObject outsideDataObject = new OutsideDataObject();
+            outsideDataObject.setDeviceId("GS00003201");
+            outsideDataObject.setDeviceGbnCd("005");
+            outsideDataObject.setOrgGbnCd("002");
+            outsideDataObject.setOrgCd("0020001");
+
+            for (int i=0; i<deviceArray.length; i++) {
+
+                System.out.println(deviceArray[i]);
+                RequestQuantumDataObject requestQuantumDataObject = new RequestQuantumDataObject();
+
+                requestQuantumDataObject.setStartDttm("2020-12-04 14:40");
+
+                //PV
+                if (deviceArray[i].equals("001")) {
+                    requestQuantumDataObject.setDeviceGbnCd(deviceArray[0]);
+                    requestQuantumDataObject.setChgrGbnCd("001");
+
+                    requestQuantumDataObjectArrayList.add(requestQuantumDataObject);
+                }
+
+                //ESS
+                else if (deviceArray[i].equals("004")) {
+                    requestQuantumDataObject.setDeviceGbnCd(deviceArray[1]);
+                    requestQuantumDataObject.setChgrGbnCd("001");
+
+                    requestQuantumDataObjectArrayList.add(requestQuantumDataObject);
+
+                    requestQuantumDataObject.setChgrGbnCd("002");
+
+                    requestQuantumDataObjectArrayList.add(requestQuantumDataObject);
+                }
+
+                //EV
+                else if (deviceArray[i].equals("005")) {
+                    requestQuantumDataObject.setDeviceGbnCd(deviceArray[2]);
+                    requestQuantumDataObject.setChgrGbnCd("001");
+                    requestQuantumDataObjectArrayList.add(requestQuantumDataObject);
+
+                }
+
+
+            }
+
+            // REST API 호출
+            ResponseObject responseObject = restTemplate.postForObject("http://172.30.1.34:8080/SharingEnergy/checkLatestData.do", outsideDataObject, ResponseObject.class);
+            System.out.println("------------------ TEST 결과 ------------------");
+            System.out.println(responseObject);
+
+
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
